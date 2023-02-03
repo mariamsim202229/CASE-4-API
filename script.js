49f6badb56eba2d3d1ba3ad96e760c890c50cd1f
mariamsim202229 <mariamsimonyan1234@gmail.com>
2023-02-03 11:17:43 +0100

    2nd commit

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 
-const search = document.querySelector ("#search");
-const button = document.querySelector ("button");
+// const search = document.querySelector ("#search");
+// const button = document.querySelector ("button");
 
-function getEarthPicture() {
-    const response = fetch('https://api.nasa.gov/EPIC/api/natural?api_key=ZO6CAcQFY8S05YpoxC3eIgz00xfouDwtbyUX4641');
-    const picture = response.json();
-    console.log(picture)
-    document.getElementById("h1").textContent = picture.date
-}
+// function getEarthPicture() {
+//     const response = fetch('https://api.nasa.gov/EPIC/api/natural?api_key=ZO6CAcQFY8S05YpoxC3eIgz00xfouDwtbyUX4641');
+//     const picture = response.json();
+//     console.log(picture)
+//     document.getElementById("h1").textContent = picture.date
+// }
 
-getEarthPicture()
+// getEarthPicture()
+
+fetch("https:api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then((response) => response.json())
+    .then((pictureData) => {
+        const copyright = pictureData.copyright;
+        const date = pictureData.date;
+        const explanation = pictureData.explanation;
+
+        const infoElement = document.createElement("div");
+        infoElement.textContent = `The date of picture is ${date} and explanation is ${explanation}.`;
+
+        document.querySelector("h2").textContent += `${copyright}`;
+
+        const pictureElement = document.getElementById("picture");
+        pictureElement.appendChild(infoElement);
+    })
+
+// .catch ((err) => {
+//     console.error (err.message);
+// });
+
+
 
 
