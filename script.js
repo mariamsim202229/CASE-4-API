64d065a5d7259eeec76794fafdd992626d2e26ad
mariamsim202229 <mariamsimonyan1234@gmail.com>
2023-02-06 09:08:02 +0100

    3rd commit

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
+const search = document.querySelector("#search");
+const button = document.querySelector("button");
 
-// const search = document.querySelector ("#search");
-// const button = document.querySelector ("button");
+button.addEventListener("search", function () {
+    console.log("klickade")
+})
+function getPictureOfTheDay() {return ''}
 
-// function getEarthPicture() {
-//     const response = fetch('https://api.nasa.gov/EPIC/api/natural?api_key=ZO6CAcQFY8S05YpoxC3eIgz00xfouDwtbyUX4641');
-//     const picture = response.json();
-//     console.log(picture)
-//     document.getElementById("h1").textContent = picture.date
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then((response) => response.json())
+        .then((pictureData) => {
+            const copyright = pictureData.copyright;
+            const date = pictureData.date;
+            const explanation = pictureData.explanation;
+            const infoElement = document.createElement("div");
+            infoElement.textContent = `The date of picture is ${date}. Explanation: ${explanation}.`;
+            document.querySelector("h2").textContent += `${copyright}`;
+
+            const pictureElement = document.getElementById("picture");
+            pictureElement.appendChild(infoElement);
+        })
+
+        // .catch((err) => {
+        //     console.error(err.message);
+        // });
+
 // }
-
-// getEarthPicture()
-
-fetch("https:api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-    .then((response) => response.json())
-    .then((pictureData) => {
-        const copyright = pictureData.copyright;
-        const date = pictureData.date;
-        const explanation = pictureData.explanation;
-
-        const infoElement = document.createElement("div");
-        infoElement.textContent = `The date of picture is ${date} and explanation is ${explanation}.`;
-
-        document.querySelector("h2").textContent += `${copyright}`;
-
-        const pictureElement = document.getElementById("picture");
-        pictureElement.appendChild(infoElement);
-    })
-
-// .catch ((err) => {
-//     console.error (err.message);
-// });
-
-
 
 
